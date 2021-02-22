355a64ad16f6128128369c10033b4e9aa57716ae
Mohamed Hussein <mohamed.hussein151515@gmail.com>
2021-02-22 22:25:38 +0200

    Initial commit

---
+++ Piu.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std ;
+int main(int argc, char** argv) {
+	
+	int utente , somma , num2 ;
+	
+	cin >> utente ;
+	
+	int prova = utente ;
+	
+	while ( prova > 0 )
+	{
+		prova = prova / 10 ;
+		somma ++ ;
+	}
+	
+	int num [ somma ] ;
+	int somma = somma 2 ;
+	
+	for ( somma ; somma > 0 ; somma -- )
+	{
+		num [ num2 ] = utente % 10
+	}
+	for ( somma2 )
+	cout << num []
+	
+	
+	return 0;
+}
